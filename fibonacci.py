fdcc4181f1a09532c671db7adb4e687e95b00de4
Oswaldo <oarincon30@ucatolica.edu.co>
2020-03-03 13:06:13 -0500

    final fibonacci ciclos y recursivo

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,7 @@
-def fibo(n):
-
-	f1 = 0
+def fiboCiclos(n):
+	f1 = 1
 	f2 = 1
-	i = 0
-
+	i = 1
 	while i < n:
 
 			ft = f1 + f2
@@ -11,14 +9,17 @@ def fibo(n):
 			f2 = ft
 			i = i + 1
 
-	print("el numero",n,"de la serie es: ", f1)
+	print("(Metodo Ciclos): El numero",n,"de la serie Fibonacci es:", f1)
 
+def fiboRecursivo(n):
+	if ((n==1) or (n==2)):
+		return 1
+	elif n>2:
+		return (fiboRecursivo(n-1)+fiboRecursivo(n-2))
 
-	pass
+num=int(input("\nEscriba un numero mayor que cero: "))
+while num < 1:
+    num=int(input("\n Escriba un numero mayor que cero: "))
 
-#def fibo_rec(n):
-#	pass
-
-
-print(fibo(6))
-#print(fibo_rec(10))
+fiboCiclos(num)
+print("(Metodo Recursivo): El numero",num,"de la serie Fibonacci es:",fiboRecursivo(num))
